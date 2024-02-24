504c3f581793026a53649be36ed6ff0efcffb5f7
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-02-24 15:06:50 +0300

    Implement stack data structure

---
+++ structures_algorithms_laboratories_2/structures/stack.py
@@ -0,0 +1,12 @@
+class Stack:
+    def __init__(self):
+        self.__items = []
+
+    def push(self, item):
+        self.__items.append(item)
+
+    def pop(self):
+        return self.__items.pop()
+    
+    def __len__(self);
+        return len(self.__items)
