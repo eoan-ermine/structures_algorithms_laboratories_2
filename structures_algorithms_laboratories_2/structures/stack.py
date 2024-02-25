7ec392ad33f7246971e3e4ccb8d1333badf625ac
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-02-25 22:12:38 +0300

    Fix typos in Stack implementation, impl few useful methods

--- structures_algorithms_laboratories_2/structures/stack.py
+++ structures_algorithms_laboratories_2/structures/stack.py
@@ -1,6 +1,8 @@
 class Stack:
-    def __init__(self):
+    def __init__(self, arr = []):
         self.__items = []
+        for e in arr:
+            self.push(e)
 
     def push(self, item):
         self.__items.append(item)
@@ -8,5 +10,11 @@ class Stack:
     def pop(self):
         return self.__items.pop()
     
-    def __len__(self);
+    def back(self):
+        return self.__items[-1]
+    
+    def __len__(self):
         return len(self.__items)
+
+    def __str__(self):
+        return str(self.__items)
