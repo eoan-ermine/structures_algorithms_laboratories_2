bc06d2a35cb9f41939d2bcd6ea1c581f828cc8a2
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-02-25 21:22:35 +0300

    Implement task_1

---
+++ structures_algorithms_laboratories_2/tasks/task_1.py
@@ -0,0 +1,36 @@
+import typer
+
+from ..structures.deque import Deque
+
+
+def decrypt(cypher: str):
+    cypher_symbols = Deque(["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"])
+    restore_deque = Deque()
+    result = ""
+
+    for symbol in cypher:
+        while cypher_symbols.front() != symbol:
+            restore_deque.push_back(cypher_symbols.front())
+            cypher_symbols.pop_front()
+        for i in range(2):
+            restore_deque.push_back(cypher_symbols.front())
+            cypher_symbols.pop_front()
+        result += cypher_symbols.front()
+
+        while len(restore_deque) != 0:
+            cypher_symbols.push_front(restore_deque.back())
+            restore_deque.pop_back()
+
+    return result
+
+
+def main(filename: str, output_filename: str):
+    with open(filename, "r") as r:
+        cypher = r.read()
+        decypher = decrypt(cypher) 
+        with open(output_filename, "w") as f:
+            print(decypher, file=f)
+
+
+def __start__():
+    typer.run(main)
